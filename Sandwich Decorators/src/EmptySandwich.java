a1796e1eaab161245f25d12d9093c3036efae747
dra7835 <dra7835@rit.edu>
2024-10-15 20:08:12 -0400

    Add constructor

--- Sandwich Decorators/src/EmptySandwich.java
+++ Sandwich Decorators/src/EmptySandwich.java
@@ -1,5 +1,6 @@
 // EmptySandwich.java
 public class EmptySandwich implements Sandwich {
+    public EmptySandwich(){}
     @Override
     public String getDescription() {
         return "Sandwich bread";
